00bd44357b6592a305ab20e049239e7dceb99769
AnwarDev2 <Anwarabdi2090@gmail.com>
2024-11-02 01:53:09 +0300

    3

---
+++ lib/third.screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ThirdScreen extends StatelessWidget {
+  final String title;
+  final String message;
+
+  const ThirdScreen({super.key, required this.title, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                message,
+                style: const TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Back'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
